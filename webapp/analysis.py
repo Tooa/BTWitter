f25a583117a7f726c4378adc6230dd871ee5a1c9
ufahrer <uli.fahrer@googlemail.com>
2014-05-02 12:42:34 +0200

    contrastive analysis refactoring part 2

--- webapp/analysis.py
+++ webapp/analysis.py
@@ -1,8 +1,8 @@
 from itertools import chain
-from collections import OrderedDict
 
-from analysisQuery import create_single_query, create_overlap_query, execute_single_analysis_query, execute_contrastive_analysis_query
-from analysisHelper import scale, transform_to_logistic_curve, remove_overlap_excess
+from analysisQuery import create_single_query, create_overlap_query, execute_single_analysis_query, \
+    execute_contrastive_analysis_query
+from analysisHelper import transform_to_logistic_curve, rescale, remove_overlap_excess, append_overlap
 from requestHelper import clean_keyword
 
 
@@ -38,38 +38,18 @@ def create_contrastive_analysis(input_values, config):
     overlap = remove_overlap_excess(overlap, overlap_limit)
     labels = list(results[0].keys()) + list(overlap.keys()) + list(results[1].keys())[::-1]
 
-    tmp = OrderedDict()
-    for k, v in overlap.items():
-        results[0][k] = (v[0], v[1])
-        tmp[k] = (v[0], v[2])
-
-    for k, v in results[1].items():
-        tmp[k] = (v[0], v[1])
-
-    results[1] = tmp
+    results = append_overlap(results, overlap)
 
     max_value = max(chain(results[0].values(), results[1].values()), key=lambda k: k[1]['measure'])[1]['measure']
     min_value = min(chain(results[0].values(), results[1].values()), key=lambda k: k[1]['measure'])[1]['measure']
 
-    data = [[transform_to_logistic_curve(results[0].get(t)[1]['measure'], max_value, min_value)
-             if t in results[0] else {'y': 0, 'org_y': 0} for t in labels],
-            [transform_to_logistic_curve(results[1].get(t)[1]['measure'], max_value, min_value)
-             if t in results[1] else {'y': 0, 'org_y': 0} for t in labels]]
+    data = transform_to_logistic_curve(results, labels, min_value, max_value)
 
     max_value_new = max(chain(data[0], data[1]), key=lambda k: k['y'])['y']
     min_value_new = min(chain(data[0], data[1]), key=lambda k: k['y'])['y']
 
-    #Oder müssen das hier die min und max der neuen daten sein?
-    small_point = int((max_value - abs(min_value)) * 0.03)
-
-    tmp1 = [
-        {'y': max(scale(d['y'], max_value, min_value, max_value_new, min_value_new), small_point), 'org_y': d['org_y']}
-        if d['org_y'] != 0 else {'y': 0, 'org_y': 0} for d in data[0]]
-    tmp2 = [{'y': max(scale(d['y'], max_value, min_value, max_value_new, min_value_new), small_point) * -1,
-             'org_y': d['org_y']}
-            if d['org_y'] != 0 else {'y': 0, 'org_y': 0} for d in data[1]]
-
-    data = [tmp1, tmp2]
+    offset = int((max_value - abs(min_value)) * 0.03)
+    data = rescale(data, min_value, max_value, min_value_new, max_value_new, offset)
 
     return labels, data, results
 
--- webapp/analysisHelper.py
+++ webapp/analysisHelper.py
@@ -13,6 +13,31 @@ def remove_overlap_excess(overlap, limit):
     return tmp
 
 
+def append_overlap(single_results, overlap):
+    tmp = OrderedDict()
+    for k, v in overlap.items():
+        single_results[0][k] = (v[0], v[1])
+        tmp[k] = (v[0], v[2])
+
+    for k, v in single_results[1].items():
+        tmp[k] = (v[0], v[1])
+
+    single_results[1] = tmp
+
+    return single_results
+
+
+def rescale(data, min_value, max_value, min_value_new, max_value_new, offset):
+    tmp1 = [
+        {'y': max(scale(d['y'], max_value, min_value, max_value_new, min_value_new), offset), 'org_y': d['org_y']}
+        if d['org_y'] != 0 else {'y': 0, 'org_y': 0} for d in data[0]]
+    tmp2 = [{'y': max(scale(d['y'], max_value, min_value, max_value_new, min_value_new), offset) * -1,
+             'org_y': d['org_y']}
+            if d['org_y'] != 0 else {'y': 0, 'org_y': 0} for d in data[1]]
+
+    return [tmp1, tmp2]
+
+
 def scale(x, origin_max, origin_min, transform_max, transform_min):
     if x == 0:
         return 0
@@ -20,10 +45,22 @@ def scale(x, origin_max, origin_min, transform_max, transform_min):
     return m * x + origin_min - m * transform_min
 
 
-def transform_to_logistic_curve(value, max_v, min_v):
-    def logic(x):
-        return 0 if x == 0 else -log(1 / x - 1)
+def logic(x):
+    return 0 if x == 0 else -log(1 / x - 1)
 
+
+def transform_data_point(value, min_v, max_v):
     #Note this will maybe return 0 values
     transformed_value = (value - min_v) / max_v
     return {'y': logic(transformed_value), 'org_y': value}
+
+
+def transform_to_logistic_curve(data, labels, min_v, max_v):
+    transformed = [
+        [transform_data_point(data[0].get(t)[1]['measure'], min_v, max_v)
+         if t in data[0] else {'y': 0, 'org_y': 0} for t in labels],
+        [transform_data_point(data[1].get(t)[1]['measure'], min_v, max_v)
+         if t in data[1] else {'y': 0, 'org_y': 0} for t in labels]
+    ]
+
+    return transformed
